42a0ed98ea54b2ecfa62c2fcf5d40e8e22981ec5
BSSE-24061 <bsse24061@itu.edu.pk>
2026-05-17 13:58:09 +0500

    push

--- backend/migrations/001_add_unique_constraint_article_likes.sql
+++
@@ -1,5 +0,0 @@
--- Add unique constraint to prevent duplicate likes on article_likes table
--- This ensures a user can only like an article once
-
-ALTER TABLE article_likes
-ADD CONSTRAINT unique_article_user_like UNIQUE (article_id, user_id);
